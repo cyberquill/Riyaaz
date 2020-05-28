6aaa71c1c14a15ab8b92a9397dfe1ec7a9895c7e
brij <brijgopalbharadwaj1999@gmail.com>
2020-05-28 19:19:18 +0530

    FS activated in electron

---
+++ .webpack.config.js
@@ -0,0 +1,6 @@
+// define child rescript
+module.exports = (config) => {
+    config.target = 'electron-renderer';
+    config.node.__dirname = true;
+    return config;
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import fs from 'fs';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const op = fs.readFileSync(__dirname+'/someData.txt', {encoding: 'utf-8'});
+        console.log(op);
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className='App-link'
+                        href='https://reactjs.org'
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
